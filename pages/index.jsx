83c9e58db095ac3873f86fdf63ba04d70fe249e2
rpjtesting <rpjtesting@gmail.com>
2022-12-05 01:30:07 +0530

    Changed component name

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,10 @@
-import { getSession, useSession } from "next-auth/react";
 import { FbHome } from "./home";
-import { Login } from "./login";
-import Link from "next/link";
 import protectedRoute from "./../utils/protectedRoute";
 
-const Home = () => {
-  const { session } = useSession();
-
+const Index = () => {
   return <FbHome />;
-  
 };
 
-export default Home;
+export default Index;
 
 export const getServerSideProps = protectedRoute();
