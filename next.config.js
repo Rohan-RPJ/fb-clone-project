0c2db5e03c71426a8e78bb4fd3af66245b61d703
rpjtesting <rpjtesting@gmail.com>
2022-12-10 13:19:35 +0530

    Added security headers

--- next.config.js
+++ next.config.js
@@ -6,6 +6,30 @@ const nextConfig = {
 
 // module.exports = nextConfig;
 
+// https://nextjs.org/docs/advanced-features/security-headers
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  // {
+  //   key: "Permissions-Policy",
+  //   value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+  // },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+];
+
 module.exports = {
   // reactStrictMode: true,
   swcMinify: true,
@@ -42,6 +66,15 @@ module.exports = {
         source: "/",
         destination: "/home",
         permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
       },
     ];
   },
